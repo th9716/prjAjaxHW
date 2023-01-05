782e0f092af0c2fae6cbd95b5654b8fdfd136fad
th9716 <lacey04849@gmail.com>
2023-01-05 22:02:17 +0800

    travel

--- prjAjaxHW/Program.cs
+++ prjAjaxHW/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=travel}/{id?}");
 
 app.Run();
