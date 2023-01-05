79d4d565f520998d152a8d911defc5fbc61eeb2b
th9716 <lacey04849@gmail.com>
2023-01-05 21:58:27 +0800

    加入專案檔案。

---
+++ prjAjaxHW/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjAjaxHW.Models;
+using System.Diagnostics;
+
+namespace prjAjaxHW.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult travel()
+        {
+            return View();
+        }
+
+
+    }
+}
